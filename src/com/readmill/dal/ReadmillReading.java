6f1e2edb9015f28bb40e11df464c5a66a3a5831f
Christoffer Klang <christoffer.klang@gmail.com>
2011-07-11 17:44:26 +0200

    Include state and is_private flag in ReadmillReading

--- src/com/readmill/dal/ReadmillReading.java
+++ src/com/readmill/dal/ReadmillReading.java
@@ -7,6 +7,8 @@ public class ReadmillReading extends ReadmillEntity {
 
   public long estimatedTimeLeft = 0;
   public double progress = 0.0;
+  public boolean isPrivate = false;
+  public int state = 1;
 
   public static interface State {
     int INTERESTING = 1;
@@ -32,6 +34,8 @@ public class ReadmillReading extends ReadmillEntity {
     id = json.optLong("id", -1);
     estimatedTimeLeft = json.optLong("estimated_time_left", 0);
     progress = json.optDouble("progress");
+    state = json.optInt("state");
+    isPrivate = json.optInt("is_private") == 1;
   }
 
   @Override public JSONObject convertToJSON() {
@@ -40,10 +44,16 @@ public class ReadmillReading extends ReadmillEntity {
       json.put("id", id);
       json.put("estimated_time_left", estimatedTimeLeft);
       json.put("progress", progress);
+      json.put("state", state);
+      json.put("is_private", isPrivate ? 1 : 0);
     } catch (JSONException e) {
       e.printStackTrace();
     }
     return json;
+  }
+
+  public int getState() {
+    return 0;
   }
 
 }
