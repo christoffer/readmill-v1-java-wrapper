85e12d00f0e4b0c77036f4879a383521497496e5
Christoffer Klang <christoffer.klang@gmail.com>
2011-07-20 15:50:30 +0200

    Add comment with missing fields on ReadmillBook

--- src/com/readmill/tests/ReadmillBookTests.java
+++ src/com/readmill/tests/ReadmillBookTests.java
@@ -7,6 +7,34 @@ import org.json.JSONObject;
 
 import com.readmill.dal.ReadmillBook;
 
+
+/*
+ TODO
+ The following fields should be accessable on instances of ReadmillBook 
+
+{
+  "id": 9,
+  "title": "Ulysses",
+  "author": "James Joyce",
+  "isbn": "9780554354088",
+  "story": "Stately- plump Buck Mulligan...",
+  "published_at": "2008-08-18",
+  "language": "en",
+  "permalink": "ulysses",
+  "permalink_url": "http://readmill.com/books/ulysses",
+  "uri": "http://api.readmill.com/books/9",
+  "cover_url": "http://static.readmill.com/covers/800-medium.png",
+  "assets": [
+    {
+      "vendor": "feedbooks",
+      "uri": "http://www.feedbooks.com/book/1232.epub",
+      "acquisition_type": 0
+    }
+  ]
+}
+
+*/
+
 public class ReadmillBookTests extends TestCase {
 
   public void testDefaults() {
