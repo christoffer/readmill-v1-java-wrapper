33a4f2438b6f69e52d35aa39418bb87ecc1df37e
Christoffer Klang <christoffer.klang@gmail.com>
2011-08-02 00:08:44 +0200

    Minor cosmetics in ReadmillTestCase

--- src/com/readmill/tests/ReadmillTestCase.java
+++ src/com/readmill/tests/ReadmillTestCase.java
@@ -9,25 +9,23 @@ import junit.framework.TestCase;
 
 /**
  * Base class for adding convenience methods to test cases
- * 
+ *
  * @author christoffer
- * 
+ *
  */
 public class ReadmillTestCase extends TestCase {
-	// The relative path of the package root where sample data is stored
+
 	private static final String testResourcePath = "/com/readmill/tests/resources/";
 
 	/**
 	 * Open a file from the support directory and return it's content.
 	 * 
-	 * @param filename
-	 *            Name of the file in the support directory
+	 * @param filename Name of the file in the support directory
 	 * @return the file content or null if the file was missing
 	 */
 	protected String getResourceContent(String resourceName) {
-		BufferedReader in = new BufferedReader(new InputStreamReader(getClass()
-				.getResourceAsStream(
-						new File(testResourcePath, resourceName).getPath())));
+	  String filePath = new File(testResourcePath, resourceName).getPath(); 
+		BufferedReader in = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(filePath)));
 		String content = "", line = null;
 		try {
 			while ((line = in.readLine()) != null) {
