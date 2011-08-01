df3a84b8a0b2b3f98757d73ae556cdbb97ac9ed1
l00pen <lovisa.skeppe@gmail.com>
2011-08-01 13:29:38 +0200

    no change, except for eclipse style...

--- src/com/readmill/tests/ReadmillTestCase.java
+++ src/com/readmill/tests/ReadmillTestCase.java
@@ -1,38 +1,44 @@
+package com.readmill.tests;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URL;
 
-import org.junit.After;
-import org.junit.Before;
+import junit.framework.TestCase;
 
-
-public class ReadmillTestCase {
-
+/**
+ * Base class for adding convenience methods to test cases
+ * 
+ * @author christoffer
+ * 
+ */
+public class ReadmillTestCase extends TestCase {
 	// The relative path of the package root where sample data is stored
-	private static final String testResourcePath = "/src/com/readmill/tests/resources";
-	 
+	private static final String testResourcePath = "/com/readmill/tests/resources/";
+
 	/**
 	 * Open a file from the support directory and return it's content.
-	 *
-	 * @param filename Name of the file in the support directory
+	 * 
+	 * @param filename
+	 *            Name of the file in the support directory
 	 * @return the file content or null if the file was missing
 	 */
-	 protected String getResourceContent(String resourceName) {
-		
-		 BufferedReader in = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(new File(resourceName).getPath())));
-		 String content = "", line = null;
-		 try {
-			 while((line = in.readLine()) != null) {
-				 content += line;
-	     }
-			 in.close();
-			 return content;
-		 } catch (IOException e) {
-			 e.printStackTrace();
-			 return null;
-	     }
-	 }
+	protected String getResourceContent(String resourceName) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(getClass()
+				.getResourceAsStream(
+						new File(testResourcePath, resourceName).getPath())));
+		String content = "", line = null;
+		try {
+			while ((line = in.readLine()) != null) {
+				content += line;
+			}
+			in.close();
+			return content;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 }
