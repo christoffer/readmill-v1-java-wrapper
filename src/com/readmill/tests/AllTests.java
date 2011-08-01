18ed173b4085a36361cca07d73c18c1013d2cc10
Christoffer Klang <christoffer.klang@gmail.com>
2011-08-02 00:10:44 +0200

    Update AllTests with added Test Cases

--- src/com/readmill/tests/AllTests.java
+++ src/com/readmill/tests/AllTests.java
@@ -8,7 +8,10 @@ public class AllTests {
   public static Test suite() {
     TestSuite suite = new TestSuite(AllTests.class.getName());
     //$JUnit-BEGIN$
+    suite.addTestSuite(ReadmillBookAssetTests.class);
     suite.addTestSuite(ReadmillBookTests.class);
+    suite.addTestSuite(ReadmillPingTests.class);
+    suite.addTestSuite(ReadmillReadingTests.class);
     suite.addTestSuite(ReadmillUserTests.class);
     //$JUnit-END$
     return suite;
