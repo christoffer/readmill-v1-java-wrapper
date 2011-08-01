222f74f3229453b712218adb4f14123d3478899d
l00pen <lovisa.skeppe@gmail.com>
2011-08-01 14:02:05 +0200

    added test on new variables corresponding to ReadmillUser

--- src/com/readmill/tests/ReadmillUserTests.java
+++ src/com/readmill/tests/ReadmillUserTests.java
@@ -1,83 +1,150 @@
 package com.readmill.tests;
 
-import junit.framework.TestCase;
+import javax.xml.datatype.DatatypeConfigurationException;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Test;
 
 import com.readmill.dal.ReadmillUser;
 
-/*
-TODO
- These are the available fields (w/ example data) that should be 
- avaialable on a user.
- 
- Taken from: https://github.com/Readmill/API/wiki/Users
+public class ReadmillUserTests extends ReadmillTestCase {
 
- {
-   "id": 2,
-   "username": "henrik",
-   "firstname": "Henrik",
-   "lastname": "Berggren",
-   "fullname": "Henrik Berggren",
-   "country": "Sweden",
-   "city": "Stockholm",
-   "website": "http://henrikberggren.com",
-   "description": "Passionate reader of internet lit and crime novels",
-   "uri": "http://api.readmill.com/users/2",
-   "permalink_url":"http://readmill.com/henrik",
-   "books_interesting":23,
-   "books_open":2,
-   "books_finished":3,
-   "books_abandoned":0,
-   "readings":"http://api.readmill.com/users/2/readings",
-   "avatar_url":"http://static.readmill.com/avatars/c4f5.png",
-   "followers":125,
-   "followings":115
- }
- 
-*/
+	private String mSampleResponse;
 
+	/**
+	 * testDefault
+	 * @throws JSONException
+	 */
+	@Test
+	public void testDefault() throws JSONException {
 
-public class ReadmillUserTests extends TestCase {
+		ReadmillUser user = new ReadmillUser();
 
-  public void testDefaultValues() {
-    ReadmillUser user = new ReadmillUser();
-    assertEquals(-1, user.id);
-    assertEquals("", user.username);
-    assertEquals("", user.fullname);
-    assertEquals(null, user.avatarURL);
-  }
-  
-  public void testConvertFromJSON() throws JSONException {
-    JSONObject json = new JSONObject(
-        "{" +
-          "'id': '45', " +
-          "'username': 'christoffer', " +
-          "'fullname': 'Christoffer Klang', " +
-          "'avatar_url': 'http://images.com/christoffer.png'" +
-        "}");
+		assertEquals("id: ", 0, user.getId());
+		assertEquals("username: ", null, user.getUserName());
+		assertEquals("fullname: ", null, user.getFullName());
+		assertEquals("avatarUrl: ", null, user.getAvatarUrl());
+		assertEquals("firstname: ", null, user.getFirstName());
+		assertEquals("lastname: ", null, user.getLastName());
+		assertEquals("country: ", null, user.getCountry());
+		assertEquals("city: ", null, user.getCity());
+		assertEquals("website: ", null, user.getWebsite());
+		assertEquals("description: ", null, user.getDescription());
+		assertEquals("uri", null, user.getUri());
+		assertEquals("permalinkUrl: ", null, user.getPermalinkUrl());
+		assertEquals("booksInteresting: ", 0, user.getBooksInteresting());
+		assertEquals("booksOpen: ", 0, user.getBooksOpen());
+		assertEquals("booksFinished: ", 0, user.getBooksFinished());
+		assertEquals("booksAbandoned: ", 0, user.getBooksAbandoned());
+		assertEquals("readings: ", null, user.getReadings());
+		assertEquals("followings: ", 0, user.getFollowings());
+		assertEquals("email: ", null, user.getEmail());
 
-    ReadmillUser user = new ReadmillUser(json);
-    assertEquals(45, user.id);
-    assertEquals("christoffer", user.username);
-    assertEquals("Christoffer Klang", user.fullname);
-    assertEquals("http://images.com/christoffer.png", user.avatarURL);
-  }
+	}
 
-  public void testConvertToJSON() throws JSONException {
-    ReadmillUser user = new ReadmillUser();
-    user.id = 45;
-    user.username = "christoffer";
-    user.fullname = "Christoffer Klang";
-    user.avatarURL = "http://images.com/christoffer.png";
+	/**
+	 * testInitFromJSON
+	 * 
+	 * @throws JSONException
+	 */
+	@Test
+	public void testInitFromJSON() throws JSONException {
+		// Load the sample JSON string from
+		// /com/readmill/tests/resources/sample_user_data.json"
+		mSampleResponse = getResourceContent("sample_user_data.json");
 
-    JSONObject json = new JSONObject(user.toJSON());
+		JSONObject json = new JSONObject(mSampleResponse);
+		ReadmillUser user = new ReadmillUser(json);
 
-    assertEquals(45, json.optInt("id"));
-    assertEquals("christoffer", json.optString("username"));
-    assertEquals("Christoffer Klang", json.optString("fullname"));
-    assertEquals("http://images.com/christoffer.png", json.optString("avatar_url"));
-  }
-  
+		assertEquals("id: ", 101, user.getId());
+		assertEquals("username: ", "jensnockert", user.getUserName());
+		assertEquals("fullname: ", "Jens Nockert", user.getFullName());
+		assertEquals("firstname: ", "Jens", user.getFirstName());
+		assertEquals("lastname: ", "Nockert", user.getLastName());
+		assertEquals("country: ", "Sweden", user.getCountry());
+		assertEquals("city: ", "Lund", user.getCity());
+		assertEquals("website: ", "", user.getWebsite());
+		assertEquals("description: ", "", user.getDescription());
+		assertEquals("uri", "http://api.readmill.com/users/101", user.getUri());
+		assertEquals("permalinkUrl: ", "http://readmill.com/jensnockert",
+				user.getPermalinkUrl());
+		assertEquals(
+				"avatarUrl: ",
+				"http://static.readmill.com/avatars/d281aa934d75016e9f60c2db7e4a55e4-medium.png?1308255920",
+				user.getAvatarUrl());
+		assertEquals("booksInteresting: ", 1, user.getBooksInteresting());
+		assertEquals("booksOpen: ", 1, user.getBooksOpen());
+		assertEquals("booksFinished: ", 0, user.getBooksFinished());
+		assertEquals("booksAbandoned: ", 0, user.getBooksAbandoned());
+		assertEquals("readings: ",
+				"http://api.readmill.com/users/101/readings",
+				user.getReadings());
+		assertEquals("followers: ", 2, user.getFollowers());
+		assertEquals("followings: ", 2, user.getFollowings());
+		assertEquals("email: ", "no@spam.plz", user.getEmail());
+	}
+
+	/**
+	 * testToConvertToJSON
+	 * 
+	 * @throws JSONException
+	 * @throws DatatypeConfigurationException
+	 */
+	@Test
+	public void testToConvertToJSON() throws JSONException,
+			DatatypeConfigurationException {
+		ReadmillUser user = new ReadmillUser();
+
+		user.setId(101);
+		user.setUserName("jensnockert");
+		user.setFullName("Jens Nockert");
+		user.setAvatarUrl("http://static.readmill.com/avatars/d281aa934d75016e9f60c2db7e4a55e4-medium.png?1308255920");
+		user.setFirstName("Jens");
+		user.setLastName("Nockert");
+		user.setCountry("Sweden");
+		user.setCity("Lund");
+		user.setWebsite("");
+		user.setDescription("");
+		user.setUri("http://api.readmill.com/users/101");
+		user.setPermalinkUrl("http://readmill.com/jensnockert");
+		user.setBooksInteresting(1);
+		user.setBooksOpen(1);
+		user.setBooksFinished(0);
+		user.setBooksAbandoned(0);
+		user.setReadings("http://api.readmill.com/users/101/readings");
+		user.setFollowers(2);
+		user.setFollowings(2);
+		user.setEmail("no@spam.plz");
+
+		JSONObject json = new JSONObject(user.toJSON());
+
+		assertEquals("id: ", 101, json.optLong("id"));
+		assertEquals("username: ", "jensnockert", json.optString("username"));
+		assertEquals("fullname: ", "Jens Nockert", json.optString("fullname"));
+		assertEquals("firstname: ", "Jens", json.optString("firstname"));
+		assertEquals("lastname: ", "Nockert", json.optString("lastname"));
+		assertEquals("country: ", "Sweden", json.optString("country"));
+		assertEquals("city: ", "Lund", json.optString("city"));
+		assertEquals("website: ", "", json.optString("website"));
+		assertEquals("description: ", "", json.optString("description"));
+		assertEquals("uri", "http://api.readmill.com/users/101",
+				json.optString("uri"));
+		assertEquals("permalinkUrl: ", "http://readmill.com/jensnockert",
+				json.optString("permalink_url"));
+		assertEquals(
+				"avatarUrl: ",
+				"http://static.readmill.com/avatars/d281aa934d75016e9f60c2db7e4a55e4-medium.png?1308255920",
+				json.optString("avatar_url"));
+		assertEquals("booksInteresting: ", 1, json.optLong("books_interesting"));
+		assertEquals("booksOpen: ", 1, json.optLong("books_open"));
+		assertEquals("booksFinished: ", 0, json.optLong("books_finished"));
+		assertEquals("booksAbandoned: ", 0, json.optLong("books_abandoned"));
+		assertEquals("readings: ",
+				"http://api.readmill.com/users/101/readings",
+				json.optString("readings"));
+		assertEquals("followers: ", 2, json.optLong("followers"));
+		assertEquals("followings: ", 2, json.optLong("followings"));
+		assertEquals("email: ", "no@spam.plz", json.optString("email"));
+	}
 }
