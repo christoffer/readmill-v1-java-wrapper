cf5ee04c9021b7e6296c784baecff920c86dea6a
Christoffer Klang <christoffer.klang@gmail.com>
2011-11-27 21:18:28 +0100

    Add USER_READINGS + Remove incorrect endpoint USERS + Cosmetics

--- src/com/readmill/api/Endpoints.java
+++ src/com/readmill/api/Endpoints.java
@@ -1,22 +1,22 @@
 package com.readmill.api;
 
 public interface Endpoints {
-    String TOKEN               = "/oauth/token";
-    String AUTHORIZE           = "/oauth/authorize";
-    String REVOKE              = "/oauth/revoke";
+  String TOKEN = "/oauth/token";
+  String AUTHORIZE = "/oauth/authorize";
+  String REVOKE = "/oauth/revoke";
 
-    // User
-    String ME                  = "/me";
-    String USERS               = "/users";
-    String USER_DETAILS        = "/users/%d";
+  // User
+  String ME = "/me";
+  String USER = "/users/%d";
+  String USER_READINGS = "/users/%d/readings";
 
-    // Book
-    String BOOKS               = "/books";
-    String BOOK_SEARCH         = "/books";
-    String BOOK_READINGS       = "/books/%d/readings";
+  // Book
+  String BOOKS = "/books";
+  String BOOK_SEARCH = "/books";
+  String BOOK_READINGS = "/books/%d/readings";
 
-    // Reading
-    String READINGS            = "/readings/%d";
-    String PINGS               = "/readings/%d/pings";
-    String PERIODS             = "/readings/%d/periods";
+  // Reading
+  String READINGS = "/readings/%d";
+  String PINGS = "/readings/%d/pings";
+  String PERIODS = "/readings/%d/periods";
 }
